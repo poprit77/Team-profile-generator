7b96ea1c8e11009b98622fa2d854a274b4c5e9c5
unknown <peterataras@gmail.com>
2023-01-17 11:14:40 -0800

    questions finished creating GHUser field

--- index.js
+++ index.js
@@ -5,160 +5,159 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const html = require("./Template/htmltenplate");
+const { createHistogram } = require("perf_hooks");
 
 const writeFileAsync = util.promisify(fs.writeFile);
-const appendFileAsync = util.promisify(fs.appendFile);
 
 let teamMemebers = [];
 let draft = ``;
-
-async function main() {
-  try {
-    await prompt();
-
-    for (let i = 0; i < teamMemebers.length; i++) {
-      draft = draft + html.generateCard(teamMemebers[i]);
-    }
-
-    let finalHTML = html.generateHTML(draft);
-
-    writeFileAsync("./result/index.html", finalHTML);
-  } catch (err) {
-    return console.log(err);
+function Builder() {
+  for (let i = 0; i < teamMemebers.length; i++) {
+    draft = draft + html.generateCard(teamMemebers[i]);
   }
+  let finalHTML = html.generateHTML(draft);
+  console.log(teamMemebers);
+  writeFileAsync("./result/index.html", finalHTML);
 }
 
-async function prompt() {
-  let responseDone = "";
-  let response2 = "";
-  let response3 = "";
-  do {
-    try {
-      response = await inquirer.prompt([
-        {
-          type: "list",
-          name: "Manager",
-          message: "Are you team manager?: ",
-          choices: ["Yes", "other"],
-        },
-      ]);
+function main() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What type of employee would you like to add to your team?",
+        name: "createEmployee",
+        choices: [
+          "Manager",
+          "Engineer",
+          "Intern",
+          "No more team members are needed.",
+        ],
+      },
+    ])
+    .then(function (userInput) {
+      switch (userInput.createEmployee) {
+        case "Manager":
+          createManager();
+          break;
+        case "Engineer":
+          createEngineer();
+          break;
+        case "Intern":
+          createIntern();
+          break;
 
-      if (response.Manager === "Yes") {
-        response2 = await inquirer.prompt([
-          {
-            type: "input",
-            name: "employeeName",
-            message: "What is the employee's name?: ",
-          },
-          {
-            type: "input",
-            name: "id",
-            message: "Enter the employee's ID: ",
-          },
-          {
-            type: "input",
-            name: "email",
-            message: "Enter the employee's email address: ",
-          },
-          {
-            type: "input",
-            name: "office",
-            message: "What is the employee's office number?:",
-          },
-        ]);
-
-        responseDone = await inquirer.prompt([
-          {
-            type: "list",
-            name: "finish",
-            message: "Do you want to continue?: ",
-            choices: ["Yes", "No"],
-          },
-        ]);
-        const manager = new Manager(
-          response2.name,
-          response2.id,
-          response2.email,
-          response2.office
-        );
-        teamMemebers.push(manager);
-       if (response.Manager === "other") {
-        response3 = await inquirer.prompt([
-          {
-            type: "list",
-            name: "type",
-            message: "what kind of employee are you?",
-            choices: ["Intern", "Engineer"]
-          },
-        ]);
-      }}
-
-      if (response3.type === "Engineer") {
-        response2 = await inquirer.prompt([
-          {
-            type: "input",
-            name: "employeeName",
-            message: "What is the employee's name?: ",
-          },
-          {
-            type: "input",
-            name: "id",
-            message: "Enter the employee's ID: ",
-          },
-          {
-            type: "input",
-            name: "email",
-            message: "Enter the employee's email address: ",
-          },
-          {
-            type: "input",
-            name: "GHUser",
-            message: "What is the employee's github username?:",
-          },
-        ]);
-        const engineer = new Engineer(
-          response.employeeName,
-          response.id,
-          response.email,
-          response2.GHUser
-        );
-        teamMemebers.push(engineer);
-      } else if (response3.type === "Intern") {
-        response4 = await inquirer.prompt([
-          {
-            type: "input",
-            name: "employeeName",
-            message: "What is the employee's name?: ",
-          },
-          {
-            type: "input",
-            name: "id",
-            message: "Enter the employee's ID: ",
-          },
-          {
-            type: "input",
-            name: "email",
-            message: "Enter the employee's email address: ",
-          },
-          {
-            type: "input",
-            name: "education",
-            message: "What school is the employee attending?:",
-          },
-        ]);
-        const intern = new Intern(
-          response.name,
-          response.id,
-          response.email,
-          response4.education
-        );
-        teamMemebers.push(intern);
+        default:
+        Builder();
       }
-    } catch (err) {
-      return console.log(err);
-    }
-    console.log(teamMemebers);
-  } while (responseDone.finish === "Yes");
+    });
+}
+
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?: ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the employee's ID: ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the employee's email address: ",
+      },
+      {
+        type: "input",
+        name: "office",
+        message: "What is the employee's office number?:",
+      },
+    ])
+    .then((response2) => {
+      const manager = new Manager(
+        response2.name,
+        response2.id,
+        response2.email,
+        response2.office
+      );
+      teamMemebers.push(manager);
+      main();
+    });
+}
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?: ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the employee's ID: ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the employee's email address: ",
+      },
+      {
+        type: "input",
+        name: "GHUser",
+        message: "What is the employee's github username?:",
+      },
+    ])
+    .then((answer) => {
+      const engineer = new Engineer(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.GHUser
+      );
+      teamMemebers.push(engineer);
+      main();
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?: ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the employee's ID: ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the employee's email address: ",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school is the employee attending?:",
+      },
+    ])
+    .then((answer) => {
+      const intern = new Intern(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.school
+      );
+      teamMemebers.push(intern);
+      main();
+    });
 }
 
 main();
